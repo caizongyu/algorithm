c595af52c23c33abd7bbc3f8de0d3462b18c22e8
caizongyu <caizongyu@qianxin.com>
2020-01-14 20:51:04 +0800

    dijkstra

--- Code/src/com/search/Dijstra.java
+++ Code/src/com/search/Dijstra.java
@@ -2,10 +2,12 @@ package com.search;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
+import java.util.Stack;
 
 /**
  * 
@@ -13,6 +15,8 @@ import java.util.Queue;
  * Dijstra 算法
  */
 public class Dijstra {
+	private static Stack<Integer> stack =new Stack<>();
+	private static String nodeString[] = {"A","b","C","D","E"};
 	//邻接矩阵
 	private static final int[][] GMAP = {
 			{-1,5,-1,7,15},
@@ -29,6 +33,9 @@ public class Dijstra {
 	static int endIndex = 4;
 	public static void main(String[] args) {
 		init();
+		doDijsktra();
+		System.err.println(Arrays.toString(pre));
+		showResult();
 		
 	}
 	//初始化 distance[] 和 pre[] 
@@ -60,14 +67,15 @@ public class Dijstra {
 			for(int i=0;i<neigberList.size();i++) {
 				//经过当前节点到相邻节点的距离  和 直接到相邻节点的距离比较
 				if(distance[currentIndex] + getDistance(currentIndex, neigberList.get(i)) < distance[neigberList.get(i)]) {
-					
+					distance[neigberList.get(i)] = distance[currentIndex] + getDistance(currentIndex, neigberList.get(i));
+					pre[neigberList.get(i)] = currentIndex;
+					queue.add(neigberList.get(i));
 				}
 			}
 		}
 		
 	}
 	private static int getDistance(Integer currentIndex, Integer targetIndex) {
-		
 		return GMAP[currentIndex][targetIndex];
 	}
 	private static List<Integer> getNeighberIndex(Integer currentIndex){
@@ -78,5 +86,23 @@ public class Dijstra {
 			}
 		}
 		return list;
-	} 
+	}
+	private static void showResult() {
+		
+		for(int i=1;i<pre.length;i++) {
+			int index=i;
+			while(index!=0) {
+				stack.add(index);
+				index = pre[index];
+			}
+			stack.add(0);
+			showIndexToString(stack);
+		}
+	}
+	private static void showIndexToString(Stack<Integer> stack) {
+		while(stack.size()!=0) {
+			System.out.print(nodeString[stack.pop()]);
+		}
+		System.out.println();
+	}
 }
